43d8e699af03457e120a63fb3b97e5ab750de4f5
mmulkeen <mulkeenm@yahoo.com>
2018-09-26 14:05:23 -0700

    Add files via upload

---
+++ newP2.cpp
@@ -0,0 +1,79 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <queue>
+#include <math.h>
+//poop
+using namespace std;
+
+void BFS( vector<int> permutation, int n );
+vector<vector<int>> findChildren( vector<int*> state );
+vector<int> reverse ( vector<int> child, int start, int end );
+bool isValid( vector<int> state );
+
+struct Node
+{
+  vector<int> key;
+  int parent;
+  Node(vector<int> x, int y){ key = x; parent = y; }
+  PrintNode(Node* node) { cout << node->key << endl; }
+
+};
+
+int main() {	
+
+  cout << "Enter a list of numbers: ";
+  
+  string ary;
+  getline(cin, ary);
+  vector<int> perm;
+  
+  string tmp;
+  for( int i = 0; i < ary.length(); i++) {
+
+    if( ary[i] == ' ' ) {
+      perm.push_back( stoi(tmp) );
+      tmp = "";
+    }
+    tmp += ary[i];
+  }
+  perm.push_back( stoi(tmp) );
+
+  
+  BFS(perm, perm.size() );
+  
+
+  return 0;
+
+}
+
+void BFS( vector<int> permutation, int n ) {
+  	vector<Node*>Pointers;
+	queue<Node*> Queue;
+	Node* start = new Node(permutation, -1);
+	Node::PrintNode(start);
+	Pointers.push_back(start);
+	//cout << Pointers.begin().key << endl;
+  
+}
+
+vector<vector<int>> findChildren( vector<int*> state ) {
+}
+
+vector<int> reverse ( vector<int> child, int start, int end ) {
+
+
+}
+
+bool isValid( vector<int> state ) {
+
+  for( int i = 0; i < state.size() - 1; i++ )
+    if( state[i] > state[i+1] )
+      return false;
+
+
+  return true;
+    
+  
+  
+}
