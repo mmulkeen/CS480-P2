f700939f62aae5ec1d2b4647158a44f0d4d2c9a7
amay723 <31995344+amay723@users.noreply.github.com>
2018-09-25 22:33:36 -0700

    Add files via upload

--- project2BFS.cpp
+++ project2BFS.cpp
@@ -35,6 +35,7 @@ int main() {
   }
   perm.push_back( stoi(tmp) );
 
+  
   BFS(perm, perm.size() );
   
 
@@ -44,52 +45,74 @@ int main() {
 
 void BFS( vector<int> permutation, int n ) {
   
-  vector<Node*> Pointers;
-  queue<Node*> Queue;
+  vector<Node> Pointers;
+  queue<Node> Queue;
 
-  Node *start;
+  Node start;
 
+  cout << "before loop\n";
+  
   for( int i = 0; i < permutation.size(); i++ ) {
-    start->key.push_back( permutation[i] );
+    cout << "iteration step: " << i << endl;
+    cout << "perm[i] " << permutation[i] << endl;
+    start.key.push_back( permutation[i] );
   }
 
-  start->parent = -1;
+  cout << "after loop\n";
+  
+
+  start.parent = -1;
 
   Pointers.push_back( start );
 
-  start->parent = 0;
+  start.parent = 0;
   
   Queue.push( start );
 
+  cout << "made it\n";
+  
   while( ! Queue.empty() ) {
 
     
-    Node *currentNode = Queue.front();
+    Node currentNode = Queue.front();
     Queue.pop();
 
-    if( isValid( currentNode->key ) ) {
+    if( isValid( currentNode.key ) ) {
       //printOutput( Pointers, currentNode.parent );
       return;
     }
 
-    vector<vector<int>> children = findChildren( currentNode->key );
+    vector<vector<int>> children = findChildren( currentNode.key );
 
-    for( auto child = children.begin(); child != children.end(); ++child ) {
+    cout << "done getting children\n";
 
-      Node *tmp;
-      tmp->key = *child;
-      tmp->parent = currentNode->parent;
-
-      Pointers.push_back( tmp );
-
-      Node *tmp2;
-      tmp2->key = *child;
-      tmp2->parent = Pointers.size() -1;
-      Queue.push( tmp2 );
+    for( int j = 0; j < children.size(); j++ ) {
+      cout << "child " << j << ' ';
+      for( int k = 0; k < children[j].size(); k++ ) {
+	cout << children[j][k] << ' ';
+      }
+      cout << endl;
       
     }
     
+    for( auto child = children.begin(); child != children.end(); ++child ) {
 
+      Node tmp;
+      tmp.key = *child;
+      tmp.parent = currentNode.parent;
+
+      Pointers.push_back( tmp );
+
+      Node tmp2;
+      tmp2.key = *child;
+      tmp2.parent = Pointers.size() -1;
+      cout << "before queue push\n";
+      Queue.push( tmp2 );
+      cout << "after queue push\n";
+      
+    }
+    
+    
 
   }
   
@@ -101,16 +124,30 @@ vector<vector<int>> findChildren( vector<int> state ) {
 
   int i = 1;
 
+  int count = 0;
+  
   while( i < state.size() ) {
     
     for(int j = 0; j < state.size() - i; j++ ) {
-    
+      count++;
       children.push_back(reverse(state, j, j+i));
 	
       i += 1;
     }
   }
 
+  cout << "total children found is " << count << endl;
+  for( int j = 0; j < children.size(); j++ ) {
+      cout << "child " << j << ' ';
+      for( int k = 0; k < children[j].size(); k++ ) {
+        cout << children[j][k] << ' ';
+      }
+      cout << endl;
+
+    }
+  cout << "end find children\n";
+
+  
   return children;
   
   
@@ -120,15 +157,43 @@ vector<int> reverse ( vector<int> child, int start, int end ) {
 
   vector<int> newA;
 
+  for( int i = 0; i < child.size(); i++ )
+    newA.push_back( child[i] );
 
-  for( int i = 0; i < (int)ceil( (end-start)/2.0); i++  ) {
+  cout << "in reverse\n";
 
+
+  end -= start;
+
+  if( end % 2 == 0 )
+    end /= 2;
+  else
+    end = end/2 + 1;
+
+  cout << "end = " << end << endl;
+  
+  for( int i = 0; i < end; i++  ) {
+
+    cout << "iteration " << i << endl;
+    
     int tmp = newA[start+i];
 
+    cout << "after tmep\n";
+    
     newA[start + i] = newA[start - i];
+    cout << "after first equal\n";
     newA[start - 1] = tmp;
+    cout << "after second\n";
   }
-      
+
+  cout << "reverse values are ";
+  for( int i = 0; i < newA.size(); i++ ) {
+    cout << newA[i] << ' ';
+  }
+  cout << endl;
+  
+  cout << "finished reverse\n";
+  
   return newA;
 
 }
