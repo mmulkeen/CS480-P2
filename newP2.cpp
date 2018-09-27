f8ce657beb662ec8dd4f1ad492bab0fac4393143
mmulkeen <mulkeenm@yahoo.com>
2018-09-27 13:02:46 -0700

    Add files via upload

--- newP2.cpp
+++ newP2.cpp
@@ -3,69 +3,104 @@
 #include <string>
 #include <queue>
 #include <math.h>
+#include <algorithm>
 //poop
 using namespace std;
 
 void BFS( vector<int> permutation, int n );
-vector<vector<int>> findChildren( vector<int*> state );
-vector<int> reverse ( vector<int> child, int start, int end );
+vector<vector<int>> findChildren( vector<int> state );
+//vector<int> reverse ( vector<int> child, int start, int end );
 bool isValid( vector<int> state );
 
 struct Node
 {
-
 	vector<int> key;
 	int parent;
 	Node(vector<int> x, int y){ key = x; parent = y; }
-	void PrintNode() { cout << key[0] << endl; }
+	void PrintNode() { 
+       		for (int i = 0; i < key.size(); i++)
+			cout<< key[i]<< ' ';
+		cout<< endl;
+	
+	}
 	vector<int> getKey() { return key; }
-
-
-
+	void SetParent(int x) {
+		parent = x;
+	}
+	void Setkey(vector<int> x) {
+		key = x;
+	}
 };
-
 int main() {	
-
-  cout << "Enter a list of numbers: ";
-  
-  string ary;
-  getline(cin, ary);
-  vector<int> perm;
-  
-  string tmp;
-  for( int i = 0; i < ary.length(); i++) {
-
-    if( ary[i] == ' ' ) {
-      perm.push_back( stoi(tmp) );
-      tmp = "";
-    }
-    tmp += ary[i];
-  }
-  perm.push_back( stoi(tmp) );
-
-  
-  BFS(perm, perm.size() );
-  
-
-  return 0;
-
+	cout << "Enter a list of numbers: ";
+	string ary;
+	getline(cin, ary);
+	vector<int> perm;
+	string tmp;
+	for( int i = 0; i < ary.length(); i++) {
+		if( ary[i] == ' ' ) {
+			perm.push_back( stoi(tmp) );
+			tmp = "";
+		}
+		tmp += ary[i];
+	}
+	perm.push_back( stoi(tmp) );
+	BFS(perm, perm.size() );
+	return 0;
 }
-
 void BFS( vector<int> permutation, int n ) {
   	vector<Node*>Pointers;
 	queue<Node*> Queue;
 	Node* start = new Node(permutation, -1);
 	Node* temp = start;
 	start->PrintNode();
-	Pointers.push_back(start); 
+	Pointers.push_back(start);
+	start->SetParent(0);
+	Queue.push(start);
+	while ( !Queue.empty()){
+		Node *currentNode = Queue.front();
+		Queue.pop();
+		if (isValid(currentNode->key)) {
+			cout << "we got it" << endl;
+			currentNode->PrintNode();
+			return;
+		}
+		vector<vector<int>> children = findChildren(currentNode->key);
+		for (auto child : children) {
+			if ( child != currentNode->key){
+				Node* tmp = new Node(child, currentNode->parent);
+				Pointers.push_back( tmp );
+				tmp->SetParent( Pointers.size()-1);
+				Queue.push( tmp );
+					
+			}
+		}
+	}
+	//cout << "Number of times loop goes threw: " << count  << endl;
 }
 
-vector<vector<int>> findChildren( vector<int*> state ) {
-}
-
-vector<int> reverse ( vector<int> child, int start, int end ) {
-
-
+vector<vector<int>> findChildren( vector<int> state ) {
+	
+	vector<vector<int>> children;
+	for ( int i = 0; i < state.size(); i++) {
+		for ( int j = i+1; j <=	state.size(); j++) {
+			vector<int> temp = state;
+			reverse(temp.begin()+i, temp.begin()+j);
+			if (temp != state)
+				children.push_back( temp );
+		}
+	}
+	/*
+	for ( int i = 0; i < children.size(); i++){
+		cout << i << ". "<< "First child: "; 
+		for ( int j = 0; j < state.size(); j++){
+			cout << children[i][j] << ' ';
+	
+		}
+		cout << endl;
+	}
+	*/
+	return children; 
 }
 
 bool isValid( vector<int> state ) {
